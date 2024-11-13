995077da7a6c47ed6be9d13cf2fee3d39407a2f7
WillKracke <wjk4218@g.rit.edu>
2024-11-13 00:37:16 -0500

    testing inv read

--- warpcontroller.lua
+++ warpcontroller.lua
@@ -1,12 +1,21 @@
-local TRANSPOSER_ID = "xxxx";
-local TRANSPOSER = component.get(TRANSPOSER_ID);
+local component = require("component");
+local sides = require("sides");
+
+local tspr = component.transposer;
 
 while true do
-    print(TRANSPOSER.getStackInSlot(1, 1));
-    print("Current Dest: ");
-    
+    print("-----");
+    print("Current Dest: " + tspr.getStackInSlot(sides.up, 1));
+    print("-----");
     print("Available Destinations: ");
+    for i = 1, tspr.getInventoryStorageSide(sides.front) do
+        local item = tspr.getStackInSlot(sides.front, i);
+        if not item == nil then
+            print(item.label);
+        end
+    end
 
+    print("=====");
     print("Input Target");
     local input = io.read();
 
